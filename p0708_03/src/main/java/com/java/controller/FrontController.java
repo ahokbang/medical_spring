cac1ae1a85cf458a3133010c184cae72455459c7
bao1019@hanmail.net <bao1019@hanmail.net>
2024-07-08 18:09:07 +0900

    스프링 세팅

---
+++ p0708_03/src/main/java/com/java/controller/FrontController.java
@@ -0,0 +1,87 @@
+package com.java.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+
+@Controller
+public class FrontController {
+	
+	// get 방식
+	@RequestMapping(value="/index", method=RequestMethod.GET)
+	public String index() {
+		// localhost:8181/index
+		// localhost:8181/WEB-INF/views/index.jsp
+		return "index";
+	}
+	
+	// get, post 방식 둘 다 사용
+	@RequestMapping("/login")
+	public String login() {		
+		return "login";
+	}
+	
+//	@RequestMapping("/board")
+//	public String board() {
+//		return "board";
+//	}
+	
+	// get 방식
+	@GetMapping("/board/board") // board 폴더 안에 board.jsp 열어줘
+	public String board() {
+		return "/board/board";
+	}
+	
+	// post 방식
+	@PostMapping("/doBoard")
+	public String doBoard() {
+//	@PostMapping("/board")
+//	public String board(int a) {
+		return "doBoard";
+	}
+	
+	@RequestMapping("/board/form")
+	public String form() {
+		return "board/form";
+	}	
+	
+	@RequestMapping("/board/doForm")
+	public String doForm(HttpServletRequest request, Model model) {
+//		model.addAttribute("bno", request.getParameter("bno"));
+//		model.addAttribute("btitle", request.getParameter("btitle"));
+		System.out.println("bno : "+request.getParameter("bno"));
+		System.out.println("btitle : "+request.getParameter("btitle"));
+		model.addAttribute("bno", request.getParameter("bno"));
+		model.addAttribute("btitle", request.getParameter("btitle"));
+		return "board/doForm";
+	}
+
+	// ===============================================================
+		@RequestMapping("/board/form2")
+		public String form2() {
+			return "board/form2";
+		}
+		
+		@RequestMapping("/board/doForm2")
+		public ModelAndView doForm2(HttpServletRequest request) {
+		System.out.println("bno : "+request.getParameter("bno"));
+		System.out.println("btitle : "+request.getParameter("btitle"));
+		
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("bno", request.getParameter("bno"));
+		mv.addObject("btitle", request.getParameter("btitle"));
+		mv.setViewName("board/doForm2");
+		
+		return mv;
+	}
+	// ===============================================================
+	
+	
+}
